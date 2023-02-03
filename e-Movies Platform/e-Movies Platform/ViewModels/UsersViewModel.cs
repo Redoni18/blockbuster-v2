15288ceac77d12e1da5f28201b36c5cb50cea721
vullnetk <vk53084@ubt-uni.net>
2023-02-03 01:59:48 +0100

    edit user profile, show users list

---
+++ e-Movies Platform/e-Movies Platform/ViewModels/UsersViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace e_Movies_Platform.ViewModels
+{
+    public class UsersViewModel
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
