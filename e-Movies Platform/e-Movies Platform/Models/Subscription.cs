dfd768d952540b34dbd0448971c2a1d8dc2d388e
RilindSylejmani <rs52543@ubt-uni.net>
2023-02-03 02:02:14 +0100

    Subscription-Crud

---
+++ e-Movies Platform/e-Movies Platform/Models/Subscription.cs
@@ -0,0 +1,9 @@
+﻿namespace e_Movies_Platform.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
