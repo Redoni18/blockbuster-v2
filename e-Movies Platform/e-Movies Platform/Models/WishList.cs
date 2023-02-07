b53e476cdeeef622a6303ccfdbe374dd02e172b0
MentorJakupi <mj52547@ubt-uni.net>
2023-02-07 20:47:13 +0100

    WishListAdded

---
+++ e-Movies Platform/e-Movies Platform/Models/WishList.cs
@@ -0,0 +1,15 @@
+﻿namespace e_Movies_Platform.Models
+{
+    public class WishList
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }    
+
+        public string? Year { get; set; }
+
+        public string? Genre { get; set; }
+
+        public bool? Isapproved { get; set; } = false;  
+
+    }
+}
